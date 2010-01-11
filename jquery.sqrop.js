a7c9a743a43ffee3712aa92eadfd4d0eeb48f8e3
Jacek Becela <jacek.becela@gmail.com>
2010-01-11 16:53:16 -0700

    Fixed rounding problems, enclosed plugin in jQuery anonymous function.

--- jquery.sqrop.js
+++ jquery.sqrop.js
@@ -9,40 +9,42 @@
     $("img").sqrop()    // square crop with side length equal to shorter side of image
     $("img").sqrop(200) // square crop with 200px side length, image scaled accordingly
 */
- 
-$.fn.sqrop = function(l){
-  return this.each(function(){
-    var e = $(this),
 
-      width = e.width(),
-      height = e.height(),
+(function(){
+  $.fn.sqrop = function(l){
+    return this.each(function(){
+      var e = $(this),
 
-      min = Math.min(width, height),
-      max = Math.max(width, height),
+        width = e.width(),
+        height = e.height(),
 
-      length = l || min,
-      ratio = length / min;
+        min = Math.min(width, height),
+        max = Math.max(width, height),
 
-      newWidth = width * ratio,
-      newHeight = height * ratio,
+        length = l || min,
+        ratio = length / min;
 
-      deltaX = (newWidth - length) / 2,
-      deltaY = (newHeight - length) / 2,
+        newWidth = Math.round(width * ratio),
+        newHeight = Math.round(height * ratio),
 
-      outer = $("<span />").css({
-        position: "relative",
-        width: length,
-        height: length,
-        display: "inline-block"
-      }),
+        deltaX = Math.round((newWidth - length) / 2),
+        deltaY = Math.round((newHeight - length) / 2),
 
-      inner = $("<span />").css({
-        position: "absolute",
-        clip: "rect(" + deltaY + "px " + (length + deltaX) + "px " + (length + deltaY) + "px " + deltaX + "px)",
-        top: -deltaY,
-        left: -deltaX
-      });
- 
-    e.css({width: newWidth, height: newHeight}).wrap(inner).parent().wrap(outer);
-  });
-}
+        outer = $("<span />").css({
+          position: "relative",
+          width: length,
+          height: length,
+          display: "inline-block"
+        }),
+
+        inner = $("<span />").css({
+          position: "absolute",
+          clip: "rect(" + deltaY + "px " + (length + deltaX) + "px " + (length + deltaY) + "px " + deltaX + "px)",
+          top: -deltaY,
+          left: -deltaX
+        });
+
+      e.css({width: newWidth, height: newHeight}).wrap(inner).parent().wrap(outer);
+    });
+  }
+})(jQuery);
